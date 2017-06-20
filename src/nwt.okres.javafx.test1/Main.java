e19ef01e0c32d45b0858c938e1fd26d41f76de9a
SifuUA <alekseykres@gmail.com>
2017-06-20 20:31:11 +0300

    Initial commit

---
+++ src/nwt.okres.javafx.test1/Main.java
@@ -0,0 +1,18 @@
+package nwt.okres.javafx.test1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Alex on 20.06.2017.
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
